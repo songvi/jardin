3d88eff89dd1b55bd767a21103d9a14da29d25ce
songvi <songvi2025@gmail.com>
2017-08-27 15:20:43 +0200

    modify talble name

--- src/User/Claims.php
+++ src/User/Claims.php
@@ -1,1 +1,25 @@
 <?php
+
+namespace Vuba\AuthN\User;
+
+class Claims
+{
+    private $sub;
+    private $name;
+    private $given_name;
+    private $family_name;
+    private $middle_name;
+    private $nickname;
+    private $preferred_username;
+    private $profile;
+    private $email;
+    private $email_verified;
+    private $gender;
+    private $birthdate;
+    private $zoneinfo;
+    private $locale;
+    private $phone_number;
+    private $address;
+    private $preferred_lang;
+    private $preferred_theme;
+}
